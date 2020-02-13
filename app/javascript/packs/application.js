bc8b9bf28dc26429c307b904876f91d35b3a4b17
ihatov08 <ihatov08@gmail.com>
2020-02-13 14:17:14 +0900

    Mod application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,5 @@ require("@rails/activestorage").start()
 require("channels")
 require("bootstrap")
 require("@fortawesome/fontawesome-free")
-require("bootstrap-datepicker")
 
 window.$ = window.jQuery = require("jquery")
