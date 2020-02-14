179777ab3681ee3f1c7f793297aab080dc3e3017
ihatov08 <ihatov08@gmail.com>
2020-02-14 15:23:58 +0900

    Execute rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_060944) do
+ActiveRecord::Schema.define(version: 2020_02_14_062114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2020_02_14_060944) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["name"], name: "index_categories_on_name", unique: true
+  end
+
+  create_table "prefectures", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "name_en", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_prefectures_on_name", unique: true
+    t.index ["name_en"], name: "index_prefectures_on_name_en", unique: true
   end
 
   create_table "users", force: :cascade do |t|
