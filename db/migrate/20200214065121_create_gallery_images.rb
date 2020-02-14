e89bb3ab6969e3e086714c732949441bc8464cad
ihatov08 <ihatov08@gmail.com>
2020-02-14 15:52:57 +0900

    Mod migration

--- db/migrate/20200214065121_create_gallery_images.rb
+++ db/migrate/20200214065121_create_gallery_images.rb
@@ -2,8 +2,8 @@ class CreateGalleryImages < ActiveRecord::Migration[6.0]
   def change
     create_table :gallery_images do |t|
       t.references :listing, null: false, foreign_key: true
-      t.string :image
-      t.integer :order
+      t.string :image, null: false
+      t.integer :order, null: false
 
       t.timestamps
     end
