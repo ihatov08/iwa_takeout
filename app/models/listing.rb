66404c4e2fd0c93f18352cfb50c36b452bc768f8
ihatov08 <ihatov08@gmail.com>
2020-02-14 15:48:10 +0900

    Add listing validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,12 @@ class Listing < ApplicationRecord
   belongs_to :category
   belongs_to :prefecture
   belongs_to :city
+
+  with_options presence: true do
+    validates :postal_code
+    validates :address
+    validates :title
+    validates :description
+    validates :price
+  end
 end
