4edd4adea244ea6c3bf5be7f32dec568e7245130
ihatov08 <ihatov08@gmail.com>
2020-02-14 15:58:36 +0900

    Mount image column

--- app/models/gallery_image.rb
+++ app/models/gallery_image.rb
@@ -1,6 +1,8 @@
 class GalleryImage < ApplicationRecord
   belongs_to :listing
 
+  mount_uploader :image, ImageUploader
+
   with_options presence: true do
     validates :image
     validates :order
