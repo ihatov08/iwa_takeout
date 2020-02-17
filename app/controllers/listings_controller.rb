35c4ab8c45a89805cb21e731e73e8155d6e08583
ihatov08 <ihatov08@gmail.com>
2020-02-17 13:19:42 +0900

    Mod listing controler

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -3,4 +3,8 @@ class ListingsController < ApplicationController
     @q = Listing.published.ransack(params[:q])
     @listings = @q.result(distinct: true).page(params[:page])
   end
+
+  def show
+    @listing = Listing.published.find(params[:id])
+  end
 end
