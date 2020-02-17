be7c9a10ab794512b21cab50c70f0dce36954a9f
ihatov08 <ihatov08@gmail.com>
2020-02-17 09:23:07 +0900

    Add association

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -13,4 +13,7 @@ class Listing < ApplicationRecord
   end
 
   mount_uploader :main_image, ImageUploader
+
+  has_many :gallery_images, dependent: :destroy
+  accepts_nested_attributes_for :gallery_images, allow_destroy: true
 end
