c5a9f8fb5d0d9eaf751a85436edf789a6226d973
ihatov08 <ihatov08@gmail.com>
2020-04-17 23:30:40 +0900

    Mod listing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "home#index"
 
   resources :listings, only: %i[index show]
+  resources :foods, only: %i[index show]
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
