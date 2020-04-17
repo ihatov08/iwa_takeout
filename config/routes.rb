668b596a7dbb383a0786b96538ffddece285e463
ihatov08 <ihatov08@gmail.com>
2020-04-17 22:55:44 +0900

    Delete user

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    confirmations:      'users/confirmations',
-    passwords:          'users/passwords',
-    registrations:      'users/registrations',
-    sessions:           'users/sessions',
-    unlocks:            'users/unlocks',
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
-
   root to: "home#index"
 
   resources :listings, only: %i[index show]
--- db/schema.rb
+++ db/schema.rb
@@ -76,37 +76,6 @@ ActiveRecord::Schema.define(version: 2020_02_14_065121) do
     t.index ["name_en"], name: "index_prefectures_on_name_en", unique: true
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "provider"
-    t.string "uid"
-    t.string "name", null: false
-    t.string "avatar"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
-  end
-
   add_foreign_key "cities", "prefectures"
   add_foreign_key "gallery_images", "listings"
   add_foreign_key "listings", "categories"
