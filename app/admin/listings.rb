09131f939061e8ca336d9f2917321d40b71b97df
ihatov08 <ihatov08@gmail.com>
2020-04-17 23:44:02 +0900

    Mod strong paramter & main image

--- app/admin/listings.rb
+++ app/admin/listings.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register Listing do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :client_id, :category_id, :postal_code, :prefecture_id, :city_id, :address, :title, :hours, :holidays, :tel, :website_url, :description, :main_image, :published
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:client_id, :category_id, :postal_code, :prefecture_id, :city_id, :address, :title, :hours, :holidays, :tel, :website_url, :description, :main_image, :published]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :client_id, :category_id, :postal_code, :prefecture_id, :city_id, :address, :title, :hours, :holidays, :tel, :website_url, :description, :main_image, :published
 end
