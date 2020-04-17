3e5e628197aa4b683fd1849bf589817a084427b1
ihatov08 <ihatov08@gmail.com>
2020-04-18 01:54:54 +0900

    Mod admin page

--- app/admin/foods.rb
+++ app/admin/foods.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Food do
   permit_params :listing_id, :name, :price, :main_image
+
+  show do
+    attributes_table do
+      row :listing
+      row :name
+      row :price
+      row :main_image do |ad|
+        image_tag ad.main_image.url
+      end
+    end
+    active_admin_comments
+  end
 end
