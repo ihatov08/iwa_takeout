12f5507dd20733f2efa60634d352ba790c747551
ihatov08 <ihatov08@gmail.com>
2020-04-20 18:02:40 +0900

    ページネーションの追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,10 @@ class HomeController < ApplicationController
     @q = Listing.published.ransack(params[:q])
     @foods = Food.joins(:listing).where("listings.published = ?", true).order(id: :desc)
     @menus = Menu.joins(:listing).where("listings.published = ?", true).order(id: :desc)
+    @listings = Listing.page(params[:page])
+
+    if params[:page]
+      redirect_to listings_path(page: params[:page])
+    end
   end
 end
