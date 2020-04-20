cff753d77d0b8a99655a28ddb3303ba0f9a84e2c
ihatov08 <ihatov08@gmail.com>
2020-04-20 14:43:55 +0900

    Add meta tag

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'cloudinary'
 gem 'activeadmin'
 gem 'high_voltage'
 gem 'google-analytics-rails'
+gem 'meta-tags'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      title:       'いわていくあうと',
+      description: '岩手県でテイクアウトができる飲食店まとめサイトです。',
+      keywords:    '岩手県,テイクアウト',
+      separator:   "&mdash;".html_safe,
+      og: {
+        title:    'いわていくあうと',
+        type:     'website',
+        description: '岩手県でテイクアウトができる飲食店まとめサイトです。',
+        url:      'https://www.iwate-takeout.com/',
+        image:    image_url("iwate.jpg")
+      }
+    }
+  end
 end
