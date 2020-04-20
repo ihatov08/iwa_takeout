af3d6c385480f7e3928220acd3fef14c91288ed0
ihatov08 <ihatov08@gmail.com>
2020-04-20 14:18:50 +0900

    Mod scraping script

--- scripts/insert_from_csv.rb
+++ scripts/insert_from_csv.rb
@@ -41,13 +41,20 @@ shops = CSV.read("shops.csv", headers: true)
 
 shops.select{ |shop| shop["address"]}.each.with_index do |shop, i|
   puts "start #{i}"
-  Listing.find_or_create_by(title: shop["shop_name"]) do |listing|
-    listing.title = shop["shop_name"]
-    listing.description = shop["biko"]
-    listing.prefecture_id = Prefecture.find_by(name: shop["address"].match_prefecture.to_s)
-    listing.city_id = City.find_by(name: shop["address"].match_city.to_s)
-    listing.address = shop["address"].extract_only_address_from_data
-    listing.website_url = shop["homepage_address"]
-    listing.google_map_url = shop["google_map_url"]
-  end
+  Listing.find_or_initialize_by(title: shop["shop_name"]).update!(
+    category_id: 1,
+    postal_code: shop["address"].postal_code_from_address_delete_kigo,
+    title: shop["shop_name"],
+    description: shop["biko"],
+    prefecture: Prefecture.find_by(name: shop["address"].match_prefecture.to_s),
+    city: City.find_by(name: shop["address"].match_city.to_s),
+    address: shop["address"].extract_only_address_from_data,
+    website_url: shop["homepage_address"],
+    google_map_url: shop["google_map_url"]
+  )
+
+rescue => e
+  p e.record
+  p e.record.errors.full_messages
+  p shop
 end
