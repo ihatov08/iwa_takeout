325e6b1783f6fe47279de5973b29a7fc979d1763
ihatov08 <ihatov08@gmail.com>
2020-04-24 23:54:09 +0900

    TabelogのURLを取り出すクラスの修正

--- scripts/fetch_tabelog_url.rb
+++ scripts/fetch_tabelog_url.rb
@@ -20,3 +20,20 @@ class Tabelog
 
   attr_reader :listing
 end
+
+file_name = "tabelog.csv"
+CSV.open(file_name, "a") do |csv|
+  unless File.exists?(file_name)
+    csv << %w(name tabelog_url)
+  end
+  Listing.find_each do |listing|
+    sleep 5
+    tabelog = Tabelog.new(listing)
+    url = tabelog.url
+    data = [listing.title, url]
+    p data
+    csv << data
+  rescue => e
+    Rails.logger.error e
+  end
+end
